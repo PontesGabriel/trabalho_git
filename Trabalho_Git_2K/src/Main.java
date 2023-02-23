01baa17a1373af5b725407e911743a09843074c1
Gabriel Pontes <pontesgabrieloliveira@gmail.com>
2023-02-23 07:59:43 -0300

    Adicionado funcionalidade inserir nome e imprimir na tela.

--- Trabalho_Git_2K/src/Main.java
+++ Trabalho_Git_2K/src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner k = new Scanner(System.in);
+		System.out.println("Insert your name: ");
+		String n = k.nextLine();
+		System.out.println("Hello "+n);
 		System.out.println("Hello World!");
 
 	}
